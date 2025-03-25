c7f8db03e4afb80e2f52e6e8e0fc238d7154a480
cherubalex <alexandresdh@gmail.com>
2025-03-25 16:56:58 +0100

    optimisation du css reste a faire les trucs dans les commit précédents

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 /** Boutons filtre pour les offres */
 
-document.getElementById("filterBtn").addEventListener("click", function() {
+document.getElementById("bouton_filtre").addEventListener("click", function() {
     //affiche les menus de filtres
-    document.getElementById("filterMenu").style.display = "block";
+    document.getElementById("filtres").style.display = "block";
     
     //on délpace les offres vers le bas pour éviter la superposition
     document.body.classList.add("show-filters");
 });
 
-document.getElementById("validateFilters").addEventListener("click", function() {
+document.getElementById("valider").addEventListener("click", function() {
     //on ferme le menu qunad la sélection est finie
-    document.getElementById("filterMenu").style.display = "none";
+    document.getElementById("filtres").style.display = "none";
     document.body.classList.remove("show-filters");
 });
