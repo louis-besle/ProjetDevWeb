8a641df62d9f4c1c73f9520dc21766c0e9f0519d
lb_sioul <louisbesle.sup@gmail.com>
2025-03-28 16:09:40 +0100

    Vérification hash des mots de passe

--- src/Controllers/SiteController.php
+++ src/Controllers/SiteController.php
@@ -15,7 +15,7 @@ class SiteController extends Controller {
     }
 
     public function _Page_Accueil(){
-        echo $this->templateEngine->render('_accueil.twig.html');
+        echo $this->templateEngine->render('_accueil.twig.html',['infos' => $this->model->getInfos()]);
     }
     public function _Page_Recherche(){
         echo $this->templateEngine->render('_recherche.twig.html');
